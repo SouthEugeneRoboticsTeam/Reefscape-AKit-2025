ecb3def844c682cd2b7c7d401db0b9a930a87d53
Drive 1/Theodore <benji.urbancic@gmail.com>
2025-01-20 19:07:22 -0800

    AAAAAAAAdvantage kit B(

---
+++ src/main/java/org/sert2521/reefscape2025/subsystems/drive/GyroIO.kt
@@ -0,0 +1,17 @@
+package org.sert2521.reefscape2025.subsystems.drive
+
+import edu.wpi.first.math.geometry.Rotation2d
+import org.team9432.annotation.Logged
+
+interface GyroIO {
+    @Logged
+    open class GyroIOInputs{
+        var connected = false
+        var yawPosition = Rotation2d()
+        var yawVelocityRadPerSec = 0.0
+        var odometryYawTimestamps = arrayOf<Double>()
+        var odometryYawPositions = arrayOf<Rotation2d>()
+    }
+
+    fun updateInputs(inputs:GyroIOInputs){}
+}
