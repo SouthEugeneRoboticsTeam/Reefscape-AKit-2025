3ab5478bd3cd45a585cab3e26191dce1fc2b20c6
Kona <conradulysses@outlook.com>
2025-02-10 18:00:39 -0800

    Added Inputs that match the regular project

--- src/main/java/org/sert2521/reefscape2025/Input.kt
+++ src/main/java/org/sert2521/reefscape2025/Input.kt
@@ -1,40 +1,124 @@
 package org.sert2521.reefscape2025
 
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.Joystick
-import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.wpilibj2.command.Commands.runOnce
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
-import org.sert2521.reefscape2025.subsystems.drivetrain.Drivetrain
+import org.sert2521.reefscape2025.commands.dispenser.DispenserOuttake
+import org.sert2521.reefscape2025.commands.dispenser.DispenserRecenter
+import org.sert2521.reefscape2025.commands.elevator.SetElevator
+import org.sert2521.reefscape2025.commands.ground_intake.Intake
+import org.sert2521.reefscape2025.commands.ground_intake.Outtake
+import org.sert2521.reefscape2025.commands.wrist.SetWrist
+import org.sert2521.reefscape2025.subsystems.dispenser.Dispenser
 
+// Bindings:
+// Gunner:
+// Stick:
+// Wrist Intake -> Trigger [1]
+// Wrist Outtake -> Thumb Bottom [2]
+// Wrist Stow -> Thumb Left [3]
+// Wrist L1 -> Thumb Right [4]
+// Left Hand:
+// Elevator L2 -> Top Right [7]
+// Elevator L3 -> Top Middle [6]
+// Elevator L4 -> Top Left [5]
+// Elevator Stow -> Bottom Left [10]
+// Dispenser (Manipulator) Reset -> Bottom Right [8]
+// Right Hand:
+// Wrist Algae -> Top Left [13]
+// Wrist Ground -> Top Middle [12]
+// Dispenser (Manipulator) Intake -> Top Right [11]
+// Toggle Automatic Intaking -> Bottom Left [14]
+// Toggle Elevator Safe Mode -> Bottom Middle [15]
+// Driver:
+// Drive -> Left Stick
+// Turn -> Right Stick
+// Reset Drivetrain -> Y
+// Wrist Outtake -> Left Bumper (LB)
+// Dispenser (Manipulator) Outtake -> Right Bumper (RB)
+// Vision Align -> X
 
 object Input {
-    val driverController = XboxController(0)
-    //val gunnerController = Joystick(1)
+    private val driverController = CommandXboxController(0)
+    private val gunnerController = Joystick(1)
 
-    private var rotationOffset = Rotation2d()
 
-    private val rotationReset = JoystickButton(driverController, 4)
+    // Button Assignment:
+    // Drivetrain:
+    private val resetDrivetrain = driverController.y()
+    private val visionAlign = driverController.x()
+
+    // Wrist:
+    private val wristGround = JoystickButton(gunnerController, 12)
+    private val wristAlgae = JoystickButton(gunnerController, 13)
+    private val wristL1 = JoystickButton(gunnerController, 4)
+    private val wristStow = JoystickButton(gunnerController, 3)
+
+    // Wrist Rollers:
+    private val wristRollerIntake = JoystickButton(gunnerController, 1)
+    private val wristRollerOuttakeDriver = driverController.leftBumper()
+    private val wristRollerOuttakeGunner = JoystickButton(gunnerController, 2)
+
+    // Elevator:
+    private val elevatorStow = JoystickButton(gunnerController, 10)
+    private val elevatorL2 = JoystickButton(gunnerController, 7)
+    private val elevatorL3 = JoystickButton(gunnerController, 6)
+    private val elevatorL4 = JoystickButton(gunnerController, 5)
+    private val toggleElevatorSafe = JoystickButton(gunnerController, 15)
+
+    // Dispenser:
+    private val toggleAutomaticIntake = JoystickButton(gunnerController, 14)
+    private val dispenserManualIntake = JoystickButton(gunnerController, 11)
+    private val dispenserOuttake = driverController.rightBumper()
+    private val dispenserReset = JoystickButton(gunnerController, 8)
+
 
     init {
-        rotationReset.onTrue(runOnce({ rotationOffset= Drivetrain.getPose().rotation}))
+
+        // rumble.onTrue(runOnce({setRumble(0.8)}).andThen(WaitCommand(0.2).andThen(runOnce({ setRumble(0.0) }))))
+
+        // Command Assignment
+        // Drivetrain
+            // resetDrivetrain.onTrue(runOnce({ Drivetrain.setPose(Pose2d()) }))
+            // visionAlign.whileTrue(VisionAlign())
+
+        // Wrist
+            wristStow.onTrue(SetWrist(SetpointConstants.WRIST_STOW))
+            wristL1.onTrue(SetWrist(SetpointConstants.WRIST_L1))
+            wristAlgae.onTrue(SetWrist(SetpointConstants.WRIST_ALGAE))
+            wristGround.onTrue(SetWrist(SetpointConstants.WRIST_GROUND))
+
+        // Wrist Rollers
+            wristRollerIntake.whileTrue(Intake())
+            wristRollerOuttakeDriver.whileTrue(Intake())
+            wristRollerOuttakeGunner.whileTrue(Outtake())
+
+        // Elevator
+            elevatorStow.onTrue(SetElevator(SetpointConstants.ELEVATOR_STOW))
+            elevatorL2.onTrue(SetElevator(SetpointConstants.ELEVATOR_L2).onlyWhile({!Dispenser.getBlocked()}))
+            elevatorL3.onTrue(SetElevator(SetpointConstants.ELEVATOR_L3).onlyWhile({!Dispenser.getBlocked()}))
+            elevatorL4.onTrue(SetElevator(SetpointConstants.ELEVATOR_L4).onlyWhile({!Dispenser.getBlocked()}))
+            // toggleElevatorSafe.onTrue(runOnce({Elevator.toggleSafeMode()}))
+
+        // Dispenser
+            // dispenserManualIntake.onTrue(DispenserManualIntake())
+            dispenserOuttake.whileTrue(DispenserOuttake())
+            dispenserReset.onTrue(DispenserRecenter())
+            // toggleAutomaticIntake.onTrue(runOnce({Dispenser.changeIntakeMode()}))
+
     }
 
+    var rotationOffset = Rotation2d(0.0)
 
-    fun getJoystickX():Double{
-        return -driverController.leftX
-    }
+    fun getJoystickX():Double{ return -driverController.leftX }
 
-    fun getJoystickY():Double{
-        return -driverController.leftY
-    }
+    fun getJoystickY():Double{ return -driverController.leftY }
 
-    fun getJoystickZ():Double {
-        return -driverController.rightX
-    }
+    fun getJoystickZ():Double { return -driverController.rightX }
 
-    fun getRotationOffset():Rotation2d{
-        return rotationOffset
-    }
+    fun getRotOffset(): Rotation2d { return rotationOffset }
+
+    fun setRumble(amount: Double) { driverController.setRumble(GenericHID.RumbleType.kBothRumble, amount) }
 }
