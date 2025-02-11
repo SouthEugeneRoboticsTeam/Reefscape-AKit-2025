08f9153095562ce6639a090bd2614baada678f5a
Drive 1/Theodore <benji.urbancic@gmail.com>
2025-02-10 16:45:59 -0800

    gradle whatevers

--- src/main/java/org/sert2521/reefscape2025/BuildConstants.java
+++ src/main/java/org/sert2521/reefscape2025/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Reefscape-AKit-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 84;
-    public static final String GIT_SHA = "92b2290bc88cb4e17a6388f990138ffac6f2efcb";
-    public static final String GIT_DATE = "2025-02-10 16:38:37 PST";
+    public static final int GIT_REVISION = 85;
+    public static final String GIT_SHA = "7f7c8b740fbab699180d8a1707ca213b8dd47f58";
+    public static final String GIT_DATE = "2025-02-10 16:44:32 PST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-02-10 16:41:32 PST";
-    public static final long BUILD_UNIX_TIME = 1739234492416L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2025-02-10 16:45:40 PST";
+    public static final long BUILD_UNIX_TIME = 1739234740234L;
+    public static final int DIRTY = 1;
 
     private BuildConstants(){}
 }
