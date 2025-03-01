10ad5da1a91e7fc4193232b4b0593793e78eb59b
Drive 1/Theodore <benji.urbancic@gmail.com>
2025-03-01 09:23:40 -0800

    Update at 'Sat Mar 01 09:23:40 PST 2025'

--- src/main/java/org/sert2521/reefscape2025/subsystems/drivetrain/SwerveConstants.kt
+++ src/main/java/org/sert2521/reefscape2025/subsystems/drivetrain/SwerveConstants.kt
@@ -70,7 +70,7 @@ object SwerveConstants {
     const val TURN_PID_MIN_INPUT = 0.0
     const val TURN_PID_MAX_INPUT = 2 * PI
 
-    val autoTranslationPID = PIDConstants(0.0, 0.0, 0.01)
+    val autoTranslationPID = PIDConstants(0.4, 0.0, 0.01)
     val autoRotationPID = PIDConstants(0.4, 0.01, 0.01, 0.7)
 
     const val VISION_ALIGN_DRIVE_P = 2.0
